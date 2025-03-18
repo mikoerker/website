976b270da1ef1bfb509807dbf5cad1204b215e51
Miko Erker <115424495+mikoerker@users.noreply.github.com>
2025-03-18 21:10:15 +0100

    Put htmls in a folder

--- mobile-zoom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.innerWidth <= 768) {
-        document.querySelector("meta[name=viewport]").setAttribute(
-            "content",
-            "width=device-width, initial-scale=0.75"
-        );
-    }
-});
